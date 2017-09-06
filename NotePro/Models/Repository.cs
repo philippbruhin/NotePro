e7afc651005618735faf36bf293715181ea809b4
philippbruhin <info@philippbruhin.ch>
2017-09-06 19:31:35 +0200

    Initial repository for notes

---
+++ NotePro/Models/Repository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NotePro.Models
+{
+    public class Repository
+    {
+
+        private static List<Note> notes = new List<Note>();
+        public static IEnumerable<Note> Notes
+        {
+            get
+            {
+                return notes;
+            }
+        }
+        public static void AddResponse(Note note)
+        {
+            notes.Add(note);
+        }
+    }
+}
