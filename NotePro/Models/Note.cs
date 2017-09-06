d80c5065f89ef6eebbabc3859aa0c14e941a4a0a
philippbruhin <info@philippbruhin.ch>
2017-09-06 19:25:50 +0200

    Initial note model

---
+++ NotePro/Models/Note.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Threading;
+
+namespace NotePro.Models
+{
+    public class Note
+    {
+
+        static int counter = 0;
+
+        public int NoteId { get; private set; }
+
+        public DateTime Creationdate { get; private set; }
+
+        public Note()
+        {
+            NoteId = Interlocked.Increment(ref counter); ;
+            Creationdate = DateTime.Now;
+        }
+
+        [Required(ErrorMessage = "Please enter a title for this task.")]
+        public string Title { get; set; }
+
+        [Required(ErrorMessage = "Please enter a description for this task.")]
+        public string Description { get; set; }
+
+        [Required(ErrorMessage = "Please enter a priority for this task.")]
+        public string Priority { get; set; }
+
+        [Required(ErrorMessage = "Please enter a duedate for this task.")]
+        public DateTime Duedate { get; set; }
+
+
+    }
+
+}
