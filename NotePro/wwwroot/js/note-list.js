9cd17f063c9171cd2942ec5ee02aaf7883dba929
philippbruhin <info@philippbruhin.ch>
2017-09-17 21:33:22 +0200

    added dot.net watch

--- NotePro/wwwroot/js/note-list.js
+++ NotePro/wwwroot/js/note-list.js
@@ -1,7 +1,7 @@
 ﻿
 var SetFilter = function () {
     var val = $("input.sort:checked").val();
-    window.open("/Home/Index?sortOptionIndex=" + val, "_self")
+    window.open("/Home/Index?sortOptionIndex=" + val, "_self");
 };
 
 $(".btn-group").children().on("change", SetFilter);
