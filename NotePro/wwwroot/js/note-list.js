ec1be4b5b2b697fee1a94a9ed3585f7e954c39e3
Leandro Rosenfelder <leandrorosenfelder@hotmail.com>
2017-09-18 19:44:08 +0200

    Fix Sorting

--- NotePro/wwwroot/js/note-list.js
+++ NotePro/wwwroot/js/note-list.js
@@ -1,6 +1,8 @@
 ﻿
 var SetFilter = function () {
+    alert("on change");
     var val = $("input.sort:checked").val();
+    alert("val " + val);
     window.open("/Home/Index?sortOptionIndex=" + val, "_self")
 };
 
