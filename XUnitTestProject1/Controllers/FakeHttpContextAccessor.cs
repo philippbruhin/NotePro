58f8734706646b2e9a29c8a4ac551c6d875f806d
philippbruhin <info@philippbruhin.ch>
2017-10-07 13:01:07 +0200

    extended httpcontext

--- XUnitTestProject1/Controllers/FakeHttpContextAccessor.cs
+++ XUnitTestProject1/Controllers/FakeHttpContextAccessor.cs
@@ -1,5 +1,13 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Claims;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Authentication;
+using Microsoft.AspNetCore.Http.Features;
 using Moq;
+
 
 namespace NotePro.Tests.Controllers
 {
@@ -9,7 +17,35 @@ namespace NotePro.Tests.Controllers
 
         public FakeHttpContextAccessor()
         {
-            HttpContext = new Mock<HttpContext>().Object;
+            HttpContext = new FakeHttpContext();
         }
     }
+
+    public class FakeHttpContext : HttpContext
+    {
+        public override IFeatureCollection Features => throw new NotImplementedException();
+
+        public override HttpRequest Request => throw new NotImplementedException();
+
+        public override HttpResponse Response => throw new NotImplementedException();
+
+        public override ConnectionInfo Connection => throw new NotImplementedException();
+
+        public override WebSocketManager WebSockets => throw new NotImplementedException();
+
+        public override AuthenticationManager Authentication => throw new NotImplementedException();
+
+        public override ClaimsPrincipal User { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public override IDictionary<object, object> Items { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public override IServiceProvider RequestServices { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public override CancellationToken RequestAborted { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public override string TraceIdentifier { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public override ISession Session { get => new Mock<ISession>().Object; set => throw new NotImplementedException(); }
+
+        public override void Abort()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
 }
